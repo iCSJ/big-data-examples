0e060228a4f37e495aa704dbb674da042bb58054
leone <exklin@gmail.com>
2018-11-26 18:16:26 +0800

    update

--- big-data-hadoop/src/main/java/com/andy/hadoop/wc/WcMain.java
+++ big-data-hadoop/src/main/java/com/andy/hadoop/wc/WcMain.java
@@ -1,5 +1,11 @@
 package com.andy.hadoop.wc;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
  * <p>
  *
@@ -8,9 +14,22 @@ package com.andy.hadoop.wc;
  **/
 public class WcMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
+        Configuration configuration = new Configuration();
+
+
+        Job job = Job.getInstance();
+        // 设置运行/处理该作业的类
+        job.setJarByClass(WcMain.class);
+        job.setJobName("WordCount");
+
+        //设置这个作业输入数据的路径
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        //设置这个作业输出结果的路径
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 
     }
