abfb5a7e5c498d8442958eaba33025b0cb2852bf
leone <exklin@gmail.com>
2018-11-26 18:19:11 +0800

    update

--- big-data-hadoop/src/main/java/com/andy/hadoop/wc/WcMain.java
+++ big-data-hadoop/src/main/java/com/andy/hadoop/wc/WcMain.java
@@ -2,6 +2,8 @@ package com.andy.hadoop.wc;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -31,6 +33,21 @@ public class WcMain {
         //设置这个作业输出结果的路径
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+        //设置实现了Map步的类
+        job.setMapperClass(WordCountMapper.class);
+
+        //设置实现了Reduce步的类
+        job.setReducerClass(WordCountReducer.class);
+
+
+        //设置输出结果key的类型
+        job.setOutputKeyClass(Text.class);
+
+        //设置输出结果value的类型
+        job.setOutputValueClass(IntWritable.class);
+
+        //执行作业
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
 
     }
 
