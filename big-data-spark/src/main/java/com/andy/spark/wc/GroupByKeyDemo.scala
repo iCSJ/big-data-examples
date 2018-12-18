e9b6fc5a6b2d70f939a610119559c7eaf5cb7ce8
leone <exklin@gmail.com>
2018-12-18 21:57:13 +0800

    feat(pom):update

---
+++ big-data-spark/src/main/java/com/andy/spark/wc/GroupByKeyDemo.scala
@@ -0,0 +1,40 @@
+package com.andy.spark.wc
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * <p>
+  *
+  * @author leone
+  * @since 2018-12-18
+  **/
+object GroupByKeyDemo {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("group-test").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val rdd1 = sc.textFile("D:\\tmp\\stu.dta")
+
+    val rdd2: RDD[(String, String)] = rdd1.map(line => {
+      val key = line.split(" ")(2)
+      (key, line)
+    })
+
+    val rdd3 = rdd2.groupByKey()
+
+    rdd3.collect().foreach(t=>{
+      val key = t._1
+      println(key + "-----------")
+      val v = t._2
+      println(v + "-----------")
+
+    })
+
+
+
+  }
+
+}
