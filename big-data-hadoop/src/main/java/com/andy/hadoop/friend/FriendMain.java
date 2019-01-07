131da301b189f296e75570c2896a8184d9a5b3c8
leone <exklin@gmail.com>
2019-01-07 18:04:33 +0800

    :art: update hadoop module

--- big-data-hadoop/src/main/java/com/andy/hadoop/friend/FriendMain.java
+++ big-data-hadoop/src/main/java/com/andy/hadoop/friend/FriendMain.java
@@ -1,11 +1,14 @@
 package com.andy.hadoop.friend;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
 import java.io.IOException;
 
@@ -17,15 +20,32 @@ import java.io.IOException;
  **/
 public class FriendMain {
 
+
+    /**
+     * mapper 业务逻辑
+     */
     static class FriendMapperOne extends Mapper<LongWritable, Text, Text, LongWritable> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String lines = value.toString();
+            String[] words = lines.split(",");
 
 
+        }
     }
 
 
+    /**
+     * reducer业务逻辑
+     */
     static class FriendReducerOnw extends Reducer<Text, LongWritable, Text, LongWritable> {
 
 
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+
+
+        }
     }
 
 
@@ -36,11 +56,21 @@ public class FriendMain {
 
 
 
+        job.setJarByClass(FriendMain.class);
+
+
+        // 如果目标文件夹存在就删除
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+
+        Path output = new Path(args[1]);
+        FileSystem fileSystem = output.getFileSystem(conf);
+        if (fileSystem.exists(output)) {
+            fileSystem.delete(output, true);
+        }
 
 
         boolean result = job.waitForCompletion(true);
         System.out.println(result ? 0 : 1);
-
     }
 
 
--- big-data-hadoop/src/main/java/com/andy/hadoop/join/side/SideJoinMain.java
+++ big-data-hadoop/src/main/java/com/andy/hadoop/join/side/SideJoinMain.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
