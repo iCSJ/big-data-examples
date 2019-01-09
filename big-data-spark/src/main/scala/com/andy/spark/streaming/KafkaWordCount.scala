588d7948af9bab80b42bea906fffa9a2493851cb
leone <exklin@gmail.com>
2019-01-09 23:45:00 +0800

    :pencil2:(spark)update

--- big-data-spark/src/main/scala/com/andy/spark/streaming/KafkaWordCount.scala
+++ big-data-spark/src/main/scala/com/andy/spark/streaming/KafkaWordCount.scala
@@ -1,7 +1,8 @@
 package com.andy.spark.streaming
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Milliseconds, StreamingContext}
 
 /**
@@ -17,17 +18,19 @@ object KafkaWordCount {
     // 设置批次产生的时间间隔
     val ssc = new StreamingContext(new SparkConf().setAppName("kafka-steaming").setMaster("local[2]"), Milliseconds(5000))
 
-    var topic = Map[String, Int]("user" -> 1)
+    var topic = Map[String, Int]("order" -> 1)
     var groupId = "group-1"
     val brokerList = "node-2:9093,node-3:9092,node-4:9092"
 
     val zkList = "node-2:2181,node-3:2181,node-4:2181"
 
     // 创建DStream
-//    val lines = KafkaUtils.createDirectStream(ssc, zkList, groupId, topic)
+    val data = KafkaUtils.createStream(ssc, zkList, groupId, topic).map(_._2)
 
+    val words = data.flatMap(_.split(" "))
+    val counts = words.map((_, 1L)).reduceByKey(_ + _)
 
-
+    counts.print()
 
     // 启动spark程序
     ssc.start()
