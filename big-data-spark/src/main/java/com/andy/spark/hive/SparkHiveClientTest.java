438a2f0f82fcb619ceb10f0a2dcd051259ca4167
leone <exklin@gmail.com>
2019-01-13 20:47:54 +0800

    :pencil2:(spark)update

---
+++ big-data-spark/src/main/java/com/andy/spark/hive/SparkHiveClientTest.java
@@ -0,0 +1,31 @@
+package com.andy.spark.hive;
+
+import java.sql.*;
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2019-01-13
+ **/
+public class SparkHiveClientTest {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.apache.hive.jdbc.HiveDriver");
+        Connection connection = DriverManager.getConnection("jdbc:hive2://node-1:10000/default");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select count(*) from t_boy");
+//        ResultSet resultSet = statement.executeQuery("select * from t_boy order by fv desc");
+
+//        System.out.println("id\tname\tfv");
+        while (resultSet.next()) {
+//            int id = resultSet.getInt("id");
+//            String name = resultSet.getString("name");
+//            int fv = resultSet.getInt("fv");
+//            System.out.println(id + "\t" + name + "\t+" + fv);
+            System.out.println(resultSet.getInt(1));
+        }
+        resultSet.close();
+    }
+
+}
